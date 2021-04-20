57bc85d671372b0cccde2645eadf07211fb9f69f
osama-alali <osama.alali89@yahoo.com>
2021-04-20 15:07:46 +0300

    lab13

--- javascript/javascrp.js
+++ javascript/javascrp.js
@@ -21,50 +21,39 @@ this.votes=0;
 this.show=0;
 arryName.push(this.name);
 product.allImage.push(this)
+
+
+
 }
 product.allImage=[];
 //console.log(product.allImage);
-new product('bag','../image/bag.jpg');
-new product('banana','image/banana.jpg');
-new product('bathroom','../image/bathroom.jpg');
-new product('boots','../image/boots.jpg');
-new product('breakfast','../image/breakfast.jpg');
-new product('bubblegum','../image/bubblegum.jpg');
-new product('chair','../image/chair.jpg');
-new product('cthulhu','../image/cthulhu.jpg');
-new product('dog-duck','../image/dog-duck.jpg');
-new product('dragon','../image/dragon.jpg');
-new product('pet-sweep','../image/pet-sweep.jpg');
-new product('scissors','../image/scissors.jpg');
-new product('shark','../image/shark.jpg');
-new product('sweep','../image/sweep.png');
-new product('tauntaun','../image/tauntaun.jpg');
-new product('unicorn','../image/unicorn.jpg');
-new product('usb','../image/usb.gif');
-new product('water-can','../image/water-can.jpg');
-new product('wine-glass','../image/wine-glass.jpg');
-new product('pen','../image/pen.jpg');
+new product('bag','./image/bag.jpg');
+new product('banana','./image/banana.jpg');
+new product('bathroom','./image/bathroom.jpg');
+new product('boots','./image/boots.jpg');
+new product('breakfast','./image/breakfast.jpg');
+new product('bubblegum','./image/bubblegum.jpg');
+new product('chair','./image/chair.jpg');
+new product('cthulhu','./image/cthulhu.jpg');
+new product('dog-duck','./image/dog-duck.jpg');
+new product('dragon','./image/dragon.jpg');
+new product('pet-sweep','./image/pet-sweep.jpg');
+new product('scissors','./image/scissors.jpg');
+new product('shark','./image/shark.jpg');
+new product('sweep','./image/sweep.png');
+new product('tauntaun','./image/tauntaun.jpg');
+new product('unicorn','./image/unicorn.jpg');
+new product('usb','./image/usb.gif');
+new product('water-can','./image/water-can.jpg');
+new product('wine-glass','./image/wine-glass.jpg');
+new product('pen','./image/pen.jpg');
 
-// function checkLastInedx()
-// {
-// if (lastIndex.includes(firstIndex)){
-//     firstIndex=genratRandomIndex();
 
-// }
-// if(lastIndex.includes(secIndex))
-// {secIndex=genratRandomIndex();}
-
-// if(lastIndex.includes(thridIndex))
-// {thridIndex=genratRandomIndex();}
-
-// }
 
 function renderThreeImage()
 {firstIndex=genratRandomIndex();
  secIndex=genratRandomIndex();
  thridIndex=genratRandomIndex();
- 
- 
  while(firstIndex===secIndex|| firstIndex===thridIndex||secIndex===thridIndex || lastIndex.includes(firstIndex) || lastIndex.includes(secIndex) || lastIndex.includes(thridIndex)) 
 {  
     firstIndex=genratRandomIndex();
@@ -99,17 +88,19 @@ if(maxAttempts >=count){
 
     }else if(event.target.id==='secound-image'){
         product.allImage[firstIndex].votes++;
-
+        
     }else if (event.target.id==='thrid-image'){
         product.allImage[thridIndex].votes++;
-
-    }console.log(product.allImage);
+        
+    }//console.log(product.allImage);
+    saveTols();
     renderThreeImage();
 }else
     {
        sectionImge.removeEventListener('click',handleClicking)
-    renderList();
-    chart();
+    //6renderList();
+    gettingVotiesAndShow();
+    //chart();
 
     }
 
@@ -127,6 +118,7 @@ function renderList()
 {
 let ul=document.getElementById('list');
 let li=null;
+ul.innerHTML='';
 for (let i =0;i<product.allImage.length;i++)
 {
    arrOfShow.push(product.allImage[i].show);
@@ -136,6 +128,29 @@ ul.appendChild(li);
 li.textContent=`${product.allImage[i].name} had ${product.allImage[i].votes} votes was seen ${product.allImage[i].show}`;
 }
 }
+
+function saveTols()
+{
+let arrstr=JSON.stringify(product.allImage);
+localStorage.setItem('productsave',arrstr);
+// console.log(product.allImage);
+// console.log(JSON.stringify(product.allImage));
+}
+
+function gettingVotiesAndShow()
+{
+let data=localStorage.getItem('productsave');
+console.log('gitelmentttt'+data);
+let item=JSON.parse(data);
+console.log('item '+ item);
+if(item !==null){
+product.allImage=item;
+renderList();
+}
+
+}
+
+
 
 function chart(){
     let ctx = document.getElementById('myChart')
@@ -161,3 +176,5 @@ function chart(){
         }
     })
     }
+
+    gettingVotiesAndShow()
